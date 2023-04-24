68f117ec053c4787be10fa8f3cb66fb0788e82e1
Alaizard <jtkopacz@gmail.com>
2023-04-24 12:34:03 -0400

    add active_storage attachment for invoice scans and all routes

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,5 +1,6 @@
 class Invoice < ApplicationRecord
   belongs_to :client
   has_many :fees, dependent: :destroy
+  has_many_attached :scans
   validates :number, presence: true, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :clients do
+    resources :invoices do
+      resources :fees, only: [:index, :create]
+      member do
+        patch :update_status
+      end
+    end
+  end
 end
